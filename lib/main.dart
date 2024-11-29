47c4c5a26b7c865fa96abe34390fd4f39b1db4ec
Aulia Sabril <kingmalprax@gmail.com>
2024-11-29 16:57:27 +0800

    first to commit

---
+++ lib/main.dart
@@ -0,0 +1,341 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const OnboardingPage1(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class OnboardingPage1 extends StatelessWidget {
+  const OnboardingPage1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: OnboardingPagePresenter(pages: [
+        OnboardingPageModel(
+          title: 'Proses Cepat Dan Aman',
+          description:
+              'gambar dari "https://www.flaticon.com/free-icons/reputation" ',
+          image: const Image(image: AssetImage('reputation.png')).toString(),
+          bgColor: Colors.indigo,
+        ),
+        OnboardingPageModel(
+          title: 'Terhubung dengan teman',
+          description:
+              'gambar dari "https://www.flaticon.com/free-icons/webinar" ',
+          image: const Image(image: AssetImage('webinar.png')).toString(),
+          bgColor: const Color(0xff1eb090),
+        ),
+        OnboardingPageModel(
+          title: 'Tandai Favorit',
+          description:
+              'gambar dari "https://www.flaticon.com/free-icons/brand-book" ',
+          image: const Image(image: AssetImage('brand-book.png')).toString(),
+          bgColor: const Color(0xfffeae4f),
+        ),
+        OnboardingPageModel(
+          title: 'Ikuti Info Terbaru',
+          description:
+              'gambar dari "https://www.flaticon.com/free-icons/personal-branding" ',
+          image: const Image(image: AssetImage('personal-branding.png'))
+              .toString(),
+          bgColor: Colors.purple,
+        ),
+      ]),
+    );
+  }
+}
+
+class OnboardingPagePresenter extends StatefulWidget {
+  final List<OnboardingPageModel> pages;
+  final VoidCallback? onSkip;
+  final VoidCallback? onFinish;
+
+  const OnboardingPagePresenter(
+      {super.key, required this.pages, this.onSkip, this.onFinish});
+
+  @override
+  State<OnboardingPagePresenter> createState() => _OnboardingPageState();
+}
+
+class _OnboardingPageState extends State<OnboardingPagePresenter> {
+  // Store the currently visible page
+  int _currentPage = 0;
+  // Define a controller for the pageview
+  final PageController _pageController = PageController(initialPage: 0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AnimatedContainer(
+        duration: const Duration(milliseconds: 250),
+        color: widget.pages[_currentPage].bgColor,
+        child: SafeArea(
+          child: Column(
+            children: [
+              Expanded(
+                // Pageview to render each page
+                child: PageView.builder(
+                  controller: _pageController,
+                  itemCount: widget.pages.length,
+                  onPageChanged: (idx) {
+                    // Change current page when pageview changes
+                    setState(() {
+                      _currentPage = idx;
+                    });
+                  },
+                  itemBuilder: (context, idx) {
+                    final item = widget.pages[idx];
+                    return Column(
+                      children: [
+                        Expanded(
+                          flex: 3,
+                          child: Padding(
+                            padding: const EdgeInsets.all(32.0),
+                            child: Image.network(
+                              item.image,
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                            flex: 1,
+                            child: Column(children: [
+                              Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Text(item.title,
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .titleLarge
+                                        ?.copyWith(
+                                          fontWeight: FontWeight.bold,
+                                          color: item.textColor,
+                                        )),
+                              ),
+                              Container(
+                                constraints:
+                                    const BoxConstraints(maxWidth: 280),
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 24.0, vertical: 8.0),
+                                child: Text(item.description,
+                                    textAlign: TextAlign.center,
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .bodyMedium
+                                        ?.copyWith(
+                                          color: item.textColor,
+                                        )),
+                              )
+                            ]))
+                      ],
+                    );
+                  },
+                ),
+              ),
+
+              // Current page indicator
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: widget.pages
+                    .map((item) => AnimatedContainer(
+                          duration: const Duration(milliseconds: 250),
+                          width: _currentPage == widget.pages.indexOf(item)
+                              ? 30
+                              : 8,
+                          height: 8,
+                          margin: const EdgeInsets.all(2.0),
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10.0)),
+                        ))
+                    .toList(),
+              ),
+
+              // Bottom buttons
+              SizedBox(
+                height: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    TextButton(
+                        style: TextButton.styleFrom(
+                            visualDensity: VisualDensity.comfortable,
+                            foregroundColor: Colors.white,
+                            textStyle: const TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold)),
+                        onPressed: () {
+                          widget.onSkip?.call();
+                        },
+                        child: const Text("Skip")),
+                    TextButton(
+                      style: TextButton.styleFrom(
+                          visualDensity: VisualDensity.comfortable,
+                          foregroundColor: Colors.white,
+                          textStyle: const TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold)),
+                      onPressed: () {
+                        if (_currentPage == widget.pages.length - 1) {
+                          widget.onFinish?.call();
+                        } else {
+                          _pageController.animateToPage(_currentPage + 1,
+                              curve: Curves.easeInOutCubic,
+                              duration: const Duration(milliseconds: 250));
+                        }
+                      },
+                      child: Row(
+                        children: [
+                          Text(
+                            _currentPage == widget.pages.length - 1
+                                ? "Finish"
+                                : "Next",
+                          ),
+                          const SizedBox(width: 8),
+                          Icon(_currentPage == widget.pages.length - 1
+                              ? Icons.done
+                              : Icons.arrow_forward),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class OnboardingPageModel {
+  final String title;
+  final String description;
+  final String image;
+  final Color bgColor;
+  final Color textColor;
+
+  OnboardingPageModel(
+      {required this.title,
+      required this.description,
+      required this.image,
+      this.bgColor = Colors.blue,
+      this.textColor = Colors.white});
+}
